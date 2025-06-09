ddd2b495775fa99c62b6a3421731f118a0e188f6
RevosCZ <72229413+RevosCZ@users.noreply.github.com>
2025-06-09 20:50:19 +0200

    Upload Buildings

---
+++ Buildings/Altar.gd
@@ -0,0 +1,21 @@
+extends StaticBody2D
+
+var mouseEntered = false
+@onready var select = get_node("Selected")
+var Selected = false
+
+func _process(delta: float) -> void:
+	select.visible = Selected
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("LeftClick"):
+		if mouseEntered == true:
+			Selected = !Selected
+			if Selected	== true:
+				Game.altarShield(position)
+
+func _on_mouse_entered() -> void:
+	mouseEntered = true
+
+func _on_mouse_exited() -> void:
+	mouseEntered = false
